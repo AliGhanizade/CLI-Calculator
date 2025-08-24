569fef22a7a763a862953468c10a658fff567199
AliGhanizade <thealighanizade@gmail.com>
2025-08-24 17:34:21 +0330

    Complete mini project

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -1,1 +1,62 @@
 package calculator
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func Calculator() {
+	var result float64
+	num1, operator, num2 := getArgs()
+	switch operator {
+	case "+":
+		result = num1 + num2
+	case "-":
+		result = num1 - num2
+	case "*":
+		result = num1 * num2
+	case "/":
+		if num2 == 0 {
+			fmt.Println("Error: Division by zero is not allowed.")
+			os.Exit(1)
+		}
+		result = num1 / num2
+		operator = "÷"
+	case "^":
+		result = math.Pow(num1, num2)
+	default:
+		fmt.Printf("Error: Invalid operator '%s'\n", operator)
+		fmt.Println("Supported operators: +, -, *, /, ^")
+		os.Exit(1)
+	}
+
+	fmt.Printf("Result: %.1f %s %.1f = %.2f\n", num1, operator, num2, result)
+
+}
+
+func getArgs() (float64, string, float64) {
+	if len(os.Args) != 4 {
+		fmt.Println("Usage: go run main.go <number1> <operator> <number2>")
+		fmt.Println("Operators: +, -, *, /, ^")
+		os.Exit(1)
+	}
+	//Get args
+	num1Str := os.Args[1]
+	operator := os.Args[2]
+	num2Str := os.Args[3]
+
+	num1, err := strconv.ParseFloat(num1Str, 64)
+	if err != nil {
+		fmt.Printf("Error: Invalid number '%s'\n", num1Str)
+		os.Exit(1)
+	}
+
+	num2, err := strconv.ParseFloat(num2Str, 64)
+	if err != nil {
+		fmt.Printf("Error: Invalid number '%s'\n", num2Str)
+		os.Exit(1)
+	}
+	return num1, operator, num2
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,11 @@
 package main
 
-func main()  {
-	
+import "calculator/calculator"
+
+func main() {
+	// This calculator works with arguments, so you need to give it arguments after execution. 
+	//for example : go run main.go 2 + 2 
+	//go run main.go 50 ^ 5
+	//go run main.go 36 / 2
+	calculator.Calculator()
 }
