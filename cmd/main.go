24e12be3bc0e3ed8c78a3d3bd2cac5792b6787ac
AliGhanizade <thealighanizade@gmail.com>
2025-10-01 22:24:39 +0330

    rename package

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,13 @@
 package main
 
-import "calculator/calculator"
+import (
+	calc "github.com/AliGhanizade/mathx/calc"
+)
 
 func main() {
 	// This calculator works with arguments, so you need to give it arguments after execution. 
 	//for example : go run main.go 2 + 2 
 	//go run main.go 50 ^ 5
 	//go run main.go 36 / 2
-	calculator.Calculator()
+	calc.Calculator()
 }
